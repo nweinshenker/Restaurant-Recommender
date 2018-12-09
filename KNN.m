ac4fbb3ccd6cd7c1c8f74ff85e479219559986ad
Michael Siem <siem@wisc.edu>
2018-12-09 15:07:28 -0600

    working on knn

--- KNN.m
+++ KNN.m
@@ -1,4 +1,4 @@
-%% load review data
+%% Load review data
 
 fid = fopen('restaurants_subset.json');
 raw = fread(fid,inf);
@@ -16,26 +16,48 @@ clear str;
 clear raw;
 clear fid;
 
+%% Make data matrix
+r_ids = fieldnames(restaurants);
+u_ids = fieldnames(users);
+users_matrix = zeros(numel(u_ids), 2);
+
+for i=1:numel(u_ids)
+  users_matrix(i, 1) = users.(u_ids{i}).average_stars;
+  users_matrix(i, 2) = users.(u_ids{i}).review_count;
+end
+
+save('user_matrix.mat', 'users_matrix', 'u_ids');
 
 
-%% Algorithm of K Nearest Neighbors
-% Given a data matrix which stores all our suggestive users preferences on
-% restaurants and a newpoint which is a simple point (1 x M).
-% 1. Find the Euclidean distance between newpoint and every point in x
-% 2. Sort all these distances in ascending order
-% 3. Return the k data points in x that are closest to newpoint
+%% Plotting
+users_list = plot_restaurant_user(4, r_ids, restaurants, u_ids, users_matrix);
 
-% Test points for the knn algorithm
-load fisheriris;
-x = meas(:,3:4);
-newpoints = [5 1.45 ; 7 2 ; 4 2.5 ; 2 3.5];
 
-%% Test Euclidean distance and Mikowski distance
-[euclid_distance] = euclid_dist(x, newpoints(1,:));
-% [mik_distance] = Mik(x(1,:), newpoints(1,:), 10);
-bsxfun(@minus, x, newpoints(1,:)).^2
-%% KNN Algorithm
+%% Access a random user from the user list
+test_index = randi(length(users_list), 1);
+test_user = users_list(test_index, :);
 
+
+
+%% KNN
+[close_users, distance] = neareset_neighbors(test_user, users_list, 1);
+
+%% Plot how we want
+figure()
+scatter(users_list(:,1), users_list(:,2));
+line(test_user(:,1),test_user(:,2),'marker','x','color','k',...
+   'markersize',10,'linewidth',2)
+
+%// radius
+r = max(distance);
+
+%// center
+c = test_user;
+
+pos = [c-r 2*r 2*r];
+rectangle('Position',pos,'Curvature',[1 1])
+
+%%
 %// Define k and the output matrices
 N = size(newpoints, 1);
 M = size(x, 2);
@@ -64,18 +86,6 @@ for i = 1 : N
     x_closest(:, :, i) = x(ind_closest(i, :), :);
 end
 
-figure();
-
-PL = meas(:,3);
-PW = meas(:,4);
-
-h1 = gscatter(PL,PW,species,'krb','ov^',[],'off');
-h1(1).LineWidth = 2;
-h1(2).LineWidth = 2;
-h1(3).LineWidth = 2;
-legend('Setosa','Versicolor','Virginica','Location','best')
-hold on;
-
 
 %% Example of training a 
 X = meas;
@@ -85,10 +95,45 @@ Mdl = fitcknn(X,Y,'NumNeighbors',5,'Standardize',1);
 Mdl.NumNeighbors = 4;
 
 
-% %% Graph the decision boundaries of the 
-% gscatter(x(:,1),x(:,2),group,'rb','+x');
-% hold on;
-% 
-% c3 = knnclassify(species, X, group, k);
-% gscatter(species(:,1),c3,'mc','o');
-% legend('Training group 1','Training group 2','Data in group 1','Data in group 2');
+%% Helper Functions
+function [users_to_plot] =  plot_restaurant_user(restaurant_i, r_ids, restaurants, u_ids, users_matrix)
+    r_id = r_ids(restaurant_i);
+    r = restaurants.(r_id{1});
+    
+    reviewfields = fieldnames(r.reviews);
+
+    users_to_plot = zeros(length(r.reviews), 2);
+    
+    for review_i = 1:length(r.reviews)
+        u_id = r.reviews(review_i).user_id;
+        u_id = strrep(string(u_id), '-', '_');
+        u_i = find(contains(u_ids,'x' + u_id));
+        if isempty(u_i)
+            u_i = find(contains(u_ids, u_id));
+        end
+        
+        users_to_plot(review_i, :) = users_matrix(u_i, :);        
+    end 
+    
+%     figure();
+%     scatter(users_to_plot(:,1), users_to_plot(:,2));
+    
+
+end
+
+function [x_closest, euc_dis] = neareset_neighbors(test_user, user_list, k)
+   %% Compute the distance between the test_user and the list of users
+    x_closest = zeros(k, size(user_list,2));
+
+    ind_closest = zeros(k, 1);
+
+    distance = euclid_dist(test_user, user_list);
+
+    %sort all the distances
+    [sorted_dist,Ind] = sort(distance); 
+    
+    ind_closest = Ind(1:k);
+    x_closest(:, :) = user_list(ind_closest(:), :);
+    euc_dis = sorted_dist(1:k);
+       
+end
