4359508ea5a6a286a4aa31e1d74c469025754617
Michael Siem <siem@wisc.edu>
2018-12-09 15:14:07 -0600

    removed unused code

--- KNN.m
+++ KNN.m
@@ -28,19 +28,15 @@ end
 
 save('user_matrix.mat', 'users_matrix', 'u_ids');
 
-
 %% Plotting
 users_list = plot_restaurant_user(4, r_ids, restaurants, u_ids, users_matrix);
-
 
 %% Access a random user from the user list
 test_index = randi(length(users_list), 1);
 test_user = users_list(test_index, :);
 
-
-
 %% KNN
-[close_users, distance] = neareset_neighbors(test_user, users_list, 1);
+[close_users, distance] = neareset_neighbors(test_user, users_list, 8);
 
 %% Plot how we want
 figure()
@@ -56,44 +52,6 @@ c = test_user;
 
 pos = [c-r 2*r 2*r];
 rectangle('Position',pos,'Curvature',[1 1])
-
-%%
-%// Define k and the output matrices
-N = size(newpoints, 1);
-M = size(x, 2);
-k = 10;
-x_closest = zeros(k, M, N);
-ind_closest = zeros(N, k);
-
-distance = zeros(150,1);
-%// Loop through each point and do logic as seen above:
-for i = 1 : N
-    %// Get the point
-    newpoint = newpoints(i, :);
-
-    
-    %// Use Euclidean
-%     for j = 1:150
-%         [dista] = euclid_dist(x(j,:), newpoint);
-%         distance(j) = dista;
-%     end
-    dists = sqrt(sum(bsxfun(@minus, x, newpoint).^2,2));
-    %sort the index for closest points
-    [d,close] = sort(dists);
-
-    %// New - Output the IDs of the match as well as the points themselves
-    ind_closest(i, :) = close(1 : k).';
-    x_closest(:, :, i) = x(ind_closest(i, :), :);
-end
-
-
-%% Example of training a 
-X = meas;
-Y = species;
-
-Mdl = fitcknn(X,Y,'NumNeighbors',5,'Standardize',1);
-Mdl.NumNeighbors = 4;
-
 
 %% Helper Functions
 function [users_to_plot] =  plot_restaurant_user(restaurant_i, r_ids, restaurants, u_ids, users_matrix)
@@ -121,8 +79,9 @@ function [users_to_plot] =  plot_restaurant_user(restaurant_i, r_ids, restaurant
 
 end
 
+
 function [x_closest, euc_dis] = neareset_neighbors(test_user, user_list, k)
-   %% Compute the distance between the test_user and the list of users
+    % Compute the distance between the test_user and the list of users
     x_closest = zeros(k, size(user_list,2));
 
     ind_closest = zeros(k, 1);
